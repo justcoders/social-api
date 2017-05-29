6020e02f5841af68ca64b1aad0c72cd8cef06046
Ivan Zmerzlyi <dangelzm@gmail.com>
2017-05-29 10:47:10 +0300

    Add main structure

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send({
+    test: 'For Test'
+  });
+});
+
+module.exports = router;
